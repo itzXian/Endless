f2e9d8dba19d168c97e72e757d3650c5530785ea
itzXian <xixian.2106@gmail.com>
2020-08-06 02:30:20 +0800

    Manually Save at Thu 06 Aug 2020 02:30:20 AM CST

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-function partA () {
+var partA = function () {
     this.src = "1.mp3";
-    this.currentTime = 11;
     var pause = ctrl.innerHTML === 'STOP';
     ctrl.innerHTML = pause ? 'PLAY' : 'STOP';
     var method = pause ? 'pause' : 'play';
-    this[method]();
+    audio[method]();
     return false;
 };
 
 ctrl = document.getElementById('playButton');
 ctrl.onclick = function () {
-    partA = this.partA;
-    partA = audio;
+    audio = new partA();
 };
 
     var doneT;
 audio.addEventListener('timeupdate', function(){
-    console.clear()
+    //console.clear()
     console.log(audio.currentTime);
     if(!doneT){
         if (audio.currentTime >= audio.duration - 0.2) {
@@ -48,3 +46,11 @@ audio.addEventListener('timeupdate', function(){
 // call click at the end
 // packge them in a new funtion
 // learn the this
+// main () {
+    //listener(clicked,parta)
+    //listener(timeupdate,-1,partb)
+    //partb () {
+        //partb
+    //}
+// };
+// merge two songs into one, use A-B loop
